9e041a04840dec2fea0d051281b0db0dd69ede0f
Ben Rolfe <benrolfe147@gmail.com>
2017-01-06 15:33:20 +0000

    Fix grammar mistake in comments

--- response.php
+++ response.php
@@ -2,8 +2,8 @@
 
 
 /*
-	In your account, set the 'Direct HTTP server-to-server request ' to 'Always online'
-	and the two fields to the URL of the is page
+	In your account, set the 'Direct HTTP server-to-server request' to 'Always online'
+	and the two fields to the URL of this page
 
 	This page will actually update the order, the customer will never see this page
 */
